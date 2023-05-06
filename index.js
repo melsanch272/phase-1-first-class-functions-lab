07d1c0e13745d53bf344cf8bd4dab99b88a7c5ca
Melissa Sanchez <melissasanchez064@gmail.com>
2023-05-06 16:37:43 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return(drivers.slice(0,2));
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return(drivers.slice(2,5));
+}
+
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (value) => {
+    return (fare) => value * value;
+}
+
+const fareDoubler = (createFareMultiplier) => {
+    return createFareMultiplier *2;
+}
+
+const fareTripler = (createFareMultiplier) => {
+    return createFareMultiplier *3;
+}
+
+function selectDifferentDrivers (drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+}
